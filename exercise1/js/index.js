08e2a05b0b84345b54917adfd03c8914d6d15d81
Amwatah Obuya <jackalltrade2020@gmail.com>
2023-01-21 15:22:15 +0300

    test one

--- exercise1/js/index.js
+++ exercise1/js/index.js
@@ -1,5 +1,10 @@
 function fibonacci(term) {
-    // TODO: Add code here
+  // TODO: Add code here
+  if (term <= 2) {
+    return 1;
+  } else {
+    return fibonacci(term - 1) + fibonacci(term - 2);
+  }
 }
 
-export default fibonacci
+export default fibonacci;
