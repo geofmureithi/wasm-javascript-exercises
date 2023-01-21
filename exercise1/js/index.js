cc0588d7cf22bdef26b238c573dfd0260eaf655d
sankaire <kerenketepela@gmail.com>
2023-01-21 15:20:59 +0300

    Chore:js fibonacci solution

--- exercise1/js/index.js
+++ exercise1/js/index.js
@@ -1,5 +1,12 @@
 function fibonacci(term) {
-    // TODO: Add code here
+    let a = 0, b = 1, result = [0, 1];
+    for (let i = 2; i <= term; i++) {
+        let c = a + b;
+        result.push(c);
+        a = b;
+        b = c;
+    }
+    return result;
 }
 
 export default fibonacci
