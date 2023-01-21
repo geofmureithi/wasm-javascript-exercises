1326037a014145f639c185f5bcd6b395c1bb9ae2
k7roman <kaizen196@gmail.com>
2023-01-21 15:22:26 +0300

    init

--- exercise1/js/index.js
+++ exercise1/js/index.js
@@ -1,5 +1,17 @@
 function fibonacci(term) {
     // TODO: Add code here
+    let n1 = 0, n2 = 1, nextTerm;
+    nextTerm = n1 + n2;
+
+    while (nextTerm <= term) {
+
+        // print the next term
+        console.log(nextTerm);
+
+        n1 = n2;
+        n2 = nextTerm;
+        nextTerm = n1 + n2;
+    }
 }
 
 export default fibonacci
