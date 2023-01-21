1f841ebef7ae25697023f7afffc4d33bbfd11962
Eric Githinji <ericgithinji@gmail.com>
2023-01-21 16:24:28 +0300

    First commit

--- exercise1/rust/index.js
+++ exercise1/rust/index.js
@@ -1,3 +1,7 @@
-import { fibonacci } from './pkg'
+import fibonacci from './pkg/rust_fibonacci.js'
+//const { fibonacci } = require('./pkg/rust_fibonacci');
+//import fibonacci from './pkg'
+//const fibonacci = require('./pkg');
 
 export default fibonacci;
+//module.exports = fibonacci;
