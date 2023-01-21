45f3a9f91061ca2944fb9ad26242e3289659d6c0
k7roman <kaizen196@gmail.com>
2023-01-21 15:40:19 +0300

    init

--- exercise1/rust/src/lib.rs
+++ exercise1/rust/src/lib.rs
@@ -10,17 +10,17 @@ use wasm_bindgen::prelude::*;
 
 
 pub fn fibonacci(term: u32) -> u32 {
-    if n < 0 {
+    if term < 0 {
         panic!("{} is negative!", term);
     }
-    match n {
-        0 => panic!("zero is not a right argument to fibonacci_reccursive()!"),
+    match term {
+        0 => panic!("zero is not a right argument to fibonacci()!"),
         1 | 2 => 1,
         3 => 2,
         /*
         50    => 12586269025,
         */
-        _ => fibonacci_reccursive(term - 1) + fibonacci_reccursive(term - 2),
+        _ => fibonacci(term - 1) + fibonacci(term - 2),
     }
 }
 
